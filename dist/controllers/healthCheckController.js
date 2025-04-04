624e697853068f76e47de310314142a9ed4170b3
Tiana1312 <odeyemisheyifunmi@gmail.com>
2025-04-04 15:26:11 +0100

    My-project

--- dist/controllers/healthCheckController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const healthCheckService = require("../services/healthCheckServices");
-exports.getHealthStatus = (req, res) => {
-    const uptimeData = healthCheckService.calculateUptime();
-    res.json(uptimeData);
-};
-//# sourceMappingURL=healthCheckController.js.map
--- src/controllers/healthCheckController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var healthCheckService = require("../services/healthCheckServices");
-exports.getHealthStatus = function (req, res) {
-    var uptimeData = healthCheckService.calculateUptime();
-    res.json(uptimeData);
-};
